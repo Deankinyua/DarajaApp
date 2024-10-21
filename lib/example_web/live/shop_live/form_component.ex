f451c1c870d7dbe0b6554a6f2f4a16fafb8fbd3f
Dean Kinyua <kinyuadean@gmail.com>
2024-10-21 06:41:52 +0300

    started adding styles to our existing form modals to at least make it look good

--- lib/example_web/live/shop_live/form_component.ex
+++ lib/example_web/live/shop_live/form_component.ex
@@ -10,83 +10,90 @@ defmodule ExampleWeb.ShopLive.FormComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      <.header>
-        <%= @title %>
-        <:subtitle>Use this form to manage shop records in your database.</:subtitle>
-      </.header>
+    <section>
+      <Layout.col>
+        <Text.title class="text-xl">
+          <Text.bold><%= @title %></Text.bold>
+        </Text.title>
 
-      <.form :let={f} for={@form} phx-target={@myself} phx-change="validate" phx-submit="save">
-        <%= if @form.source.type == :create do %>
-          <Layout.col class="space-y-1.5">
-            <label for="name_field">
-              <Text.text class="text-tremor-content">
-                Shop Name
-              </Text.text>
-            </label>
+        <Text.subtitle color="gray">
+          Use this form to manage shop records in your database.
+        </Text.subtitle>
 
-            <Input.text_input
-              id="name"
-              name={@form[:name].name}
-              placeholder="Shop Name..."
-              type="text"
-              field={@form[:name]}
-              value={@form[:name].value}
-              required="true"
-            />
-          </Layout.col>
+        <Layout.divider class="my-4" />
 
-          <Layout.col class="space-y-1.5">
-            <label for="region[region_id]">
-              <Text.text class="text-tremor-content">
-                Region Name
-              </Text.text>
-            </label>
+        <.form :let={f} for={@form} phx-target={@myself} phx-change="validate" phx-submit="save">
+          <%= if @form.source.type == :create do %>
+            <Layout.col class="space-y-1.5">
+              <label for="name_field">
+                <Text.text class="text-tremor-content">
+                  Shop Name
+                </Text.text>
+              </label>
 
-            <.input type="select" field={f[:region_id]} options={@region_selector} />
-          </Layout.col>
-        <% end %>
+              <Input.text_input
+                id="name"
+                name={@form[:name].name}
+                placeholder="Shop Name..."
+                type="text"
+                field={@form[:name]}
+                value={@form[:name].value}
+                required="true"
+              />
+            </Layout.col>
 
-        <%= if @form.source.type == :update do %>
-          <Layout.col class="space-y-1.5">
-            <label for="name_field">
-              <Text.text class="text-tremor-content">
-                Shop Name
-              </Text.text>
-            </label>
+            <Layout.col class="space-y-1.5">
+              <label for="region[region_id]">
+                <Text.text class="text-tremor-content">
+                  Region Name
+                </Text.text>
+              </label>
 
-            <Input.text_input
-              disabled
-              id="name"
-              name={@form[:name].name}
-              placeholder="Shop Name..."
-              type="text"
-              field={@form[:name]}
-              value={@form[:name].value}
-              required="true"
-            />
-          </Layout.col>
-
-          <Layout.col class="space-y-1.5">
-            <label for="region[region_id]">
-              <Text.text class="text-tremor-content">
-                Region Name
-              </Text.text>
-            </label>
-
-            <.input type="select" field={f[:region_id]} options={@region_selector} />
-          </Layout.col>
-        <% end %>
-
-        <Button.button type="submit" size="xl" class="mt-2 w-min" phx-disable-with="Saving...">
-          <%= if @form.source.type == :update do %>
-            Update Shop
-          <% else %>
-            Create Shop
+              <.input type="select" field={f[:region_id]} options={@region_selector} />
+            </Layout.col>
           <% end %>
-        </Button.button>
-      </.form>
-    </div>
+
+          <%= if @form.source.type == :update do %>
+            <Layout.col class="space-y-1.5">
+              <label for="name_field">
+                <Text.text class="text-tremor-content">
+                  Shop Name
+                </Text.text>
+              </label>
+
+              <Input.text_input
+                disabled
+                id="name"
+                name={@form[:name].name}
+                placeholder="Shop Name..."
+                type="text"
+                field={@form[:name]}
+                value={@form[:name].value}
+                required="true"
+              />
+            </Layout.col>
+
+            <Layout.col class="space-y-1.5">
+              <label for="region[region_id]">
+                <Text.text class="text-tremor-content">
+                  Region Name
+                </Text.text>
+              </label>
+
+              <.input type="select" field={f[:region_id]} options={@region_selector} />
+            </Layout.col>
+          <% end %>
+
+          <Button.button type="submit" size="xl" class="mt-2 w-min" phx-disable-with="Saving...">
+            <%= if @form.source.type == :update do %>
+              Update Shop
+            <% else %>
+              Create Shop
+            <% end %>
+          </Button.button>
+        </.form>
+      </Layout.col>
+    </section>
     """
   end
 
@@ -162,3 +169,173 @@ defmodule ExampleWeb.ShopLive.FormComponent do
     end
   end
 end
+
+# ~H"""
+
+#     <Layout.col>
+#       <Text.title class="text-xl">
+#         <Text.bold><%= @title %></Text.bold>
+#       </Text.title>
+
+#       <Text.subtitle color="gray">
+#         Use this form to manage organization records in your database.
+#       </Text.subtitle>
+
+#       <Layout.divider class="my-4" />
+
+#       <.form
+#         for={@form}
+#         id="organization-form"
+#         class={
+#           Tails.classes([
+#             "w-full py-4",
+#             @submitting && "pointer-events-none"
+#           ])
+#         }
+#         phx-target={@myself}
+#         phx-change="validate"
+#         phx-submit="submit"
+#       >
+#         <Layout.grid class="gap-3">
+#           <div :if={@errors != nil} class="danger" role="alert">
+#             <Text.subtitle color="rose">
+#               <Text.bold>Something went wrong</Text.bold>
+#             </Text.subtitle>
+#             <ul class="error-messages">
+#               <li>
+#                 <Text.text>A required field is missing</Text.text>
+#               </li>
+#             </ul>
+#           </div>
+
+#           <fieldset class="hidden pointer-events-none">
+#             <Input.text_input
+#               id="organization"
+#               name="organization[user_id]"
+#               placeholder="user_id"
+#               type="text"
+#               field={@form[:user_id]}
+#               value={@current_user.id}
+#             />
+#           </fieldset>
+
+#           <%= if @form.source.type == :create do %>
+#             <Layout.col class="space-y-1.5">
+#               <label for="name_field">
+#                 <Text.text class="text-tremor-content">
+#                   Name
+#                 </Text.text>
+#               </label>
+
+#               <Input.text_input
+#                 id="name"
+#                 name={@form[:name].name}
+#                 placeholder="Enter value"
+#                 type="text"
+#                 field={@form[:name]}
+#                 value={@form[:name].value}
+#                 error={@form[:name].errors != [] && field_touched(@form[:name], :name) == true}
+#                 error_message={get_field_errors(@form[:name], :name)}
+#                 required="true"
+#               />
+#             </Layout.col>
+
+#             <Layout.col class="space-y-1.5">
+#               <label for="email_field">
+#                 <Text.text class="text-tremor-content">
+#                   Email
+#                 </Text.text>
+#               </label>
+
+#               <Input.text_input
+#                 id="email"
+#                 name={@form[:email].name}
+#                 placeholder="myorganization@example.com"
+#                 type="text"
+#                 field={@form[:email]}
+#                 value={@form[:email].value}
+#                 error={@form[:email].errors != [] && field_touched(@form[:email], :name) == true}
+#                 error_message={get_field_errors(@form[:email], :email)}
+#                 required="true"
+#               />
+#             </Layout.col>
+#           <% end %>
+#           <%= if @form.source.type == :update do %>
+#             <Layout.col class="space-y-1.5">
+#               <label for="name_field">
+#                 <Text.text class="text-tremor-content">
+#                   Name
+#                 </Text.text>
+#               </label>
+
+#               <Input.text_input
+#                 id="name"
+#                 name={@form[:name].name}
+#                 placeholder="Enter value"
+#                 type="text"
+#                 field={@form[:name]}
+#                 value={@form[:name].value}
+#                 error={@form[:name].errors != [] && field_touched(@form[:name], :name) == true}
+#                 error_message={get_field_errors(@form[:name], :name)}
+#                 required="true"
+#               />
+#             </Layout.col>
+
+#             <Layout.col class="space-y-1.5">
+#               <label for="email_field">
+#                 <Text.text class="text-tremor-content">
+#                   Email
+#                 </Text.text>
+#               </label>
+
+#               <Input.text_input
+#                 id="email"
+#                 name={@form[:email].name}
+#                 placeholder="Enter value"
+#                 type="text"
+#                 field={@form[:email]}
+#                 value={@form[:email].value}
+#                 error={@form[:email].errors != [] && field_touched(@form[:email], :name) == true}
+#                 error_message={get_field_errors(@form[:email], :email)}
+#                 required="true"
+#               />
+#             </Layout.col>
+#             <Layout.col class="space-y-1.5">
+#               <label for={@form[:status].name <> "_field"}>
+#                 <Text.text class="text-tremor-content">
+#                   Status
+#                 </Text.text>
+#               </label>
+
+#               <Select.select
+#                 id="status"
+#                 name={@form[:status].name}
+#                 placeholder="Select..."
+#                 value={@form[:status].value}
+#               >
+#                 <:icon>
+#                   <.icon name="hero-cog" />
+#                 </:icon>
+
+#                 <:item :for={
+#                   value <-
+#                     Ash.Resource.Info.attribute(Songanote.Accounts.Organization, :status).constraints[
+#                       :one_of
+#                     ]
+#                 }>
+#                   <%= value %>
+#                 </:item>
+#               </Select.select>
+#             </Layout.col>
+#           <% end %>
+
+#           <Layout.divider class="my-0 mt-8" />
+
+#           <Button.button loading={@submitting} type="submit" size="xl" class="mt-2 w-min">
+#             Create Organization
+#           </Button.button>
+#         </Layout.grid>
+#       </.form>
+#     </Layout.col>
+
+# """
